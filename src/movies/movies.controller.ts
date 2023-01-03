44b59e4c11051bb75dd085782dbf723d2066fe97
yena choi <geniusyena332@gmail.com>
2023-01-03 15:29:25 +0900

    FIX: NotFoundException imported and delete HttpException

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,17 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Delete,
-  Patch,
-  Param,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, Post, Delete, Param, Body } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entities';
 import { MoviesService } from './movies.service';
 
-@Controller('movies')
+@Controller('api')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entities';
 
