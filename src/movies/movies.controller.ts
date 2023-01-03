1c3a221c2cc605d28ebf477566d7b2af0d9a836d
bsh714 <natebsh@nate.com>
2023-01-03 15:52:02 +0900

    On branch bsh 타입스크립트 포스트
     Changes to be committed:
    	modified:   src/movies/movies.controller.ts
    	modified:   src/movies/movies.service.ts

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -6,4 +6,12 @@ import { MoviesService } from './movies.service';
 @Controller('api')
 export class MoviesController {
   constructor(private readonly moviesService: MoviesService) {}
+  @Post()
+  create(@Body() movieData) {
+    return this.moviesService.create(movieData);
+  }
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
 }
--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -3,4 +3,16 @@ import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entities';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+    private movies: Movie[] = [];
+    
+    create(movieData) {
+        this.movies.push({
+          id: this.movies.length + 1,
+          ...movieData,
+        });
+      }
+     getAll(): Movie[] {
+        return this.movies;
+      }
+}
