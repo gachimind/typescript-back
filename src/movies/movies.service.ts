01fa9743e3977bd7528574b466bf6992fe0c58c3
DY <p930601@gmail.com>
2023-01-03 14:50:37 +0700

    ✨ FEAT : Add Features (getAll)

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -3,4 +3,12 @@ import { CreateMovieDto } from './dto/create-movie.dto';
 import { Movie } from './entities/movie.entities';
 
 @Injectable()
-export class MoviesService {}
+export class MoviesService {
+
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+}
